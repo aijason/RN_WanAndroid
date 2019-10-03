d42b80f2cf8ac9382772e066d5427a07c992a3ec
aijason <977854695@qq.com>
2019-10-03 11:16:33 +0800

    Update App.js

--- App.js
+++ App.js
@@ -2,13 +2,10 @@ import React, {PureComponent} from 'react';
 import {View, StatusBar} from 'react-native';
 import {Provider} from 'react-redux';
 import SplashScreen from 'react-native-splash-screen';
-// import {PersistGate} from 'redux-persist/integration/react';
 import AppNavigator from './src';
 import {setAxios} from './src/service/setAxios';
 import Toast from './src/utils/Toast';
 import store from './src/store';
-
-// const {store, persistor} = configureStore();
 
 class App extends PureComponent {
   componentDidMount() {
@@ -21,13 +18,11 @@ class App extends PureComponent {
   render() {
     return (
       <Provider store={store}>
-        {/*<PersistGate loading={null} persistor={persistor}>*/}
         <View style={{flex: 1}}>
           <StatusBar barStyle={'light-content'} translucent={true} />
           <AppNavigator />
           <Toast ref="toast" />
         </View>
-        {/*</PersistGate>*/}
       </Provider>
     );
   }
